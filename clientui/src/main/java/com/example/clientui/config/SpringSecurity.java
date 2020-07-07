0de017a523ef40bba75623053efb6aa40952ae44
charlotte <vanhuyse.charlotte@gmail.com>
2020-07-07 09:23:08 -0400

    update client

--- clientui/src/main/java/com/example/clientui/config/SpringSecurity.java
+++ clientui/src/main/java/com/example/clientui/config/SpringSecurity.java
@@ -1,29 +1,22 @@
 package com.example.clientui.config;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@EnableOAuth2Sso
 @Configuration
 public class SpringSecurity extends WebSecurityConfigurerAdapter {
 
-    
-
-  /*  @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .antMatcher("/**")
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .anyRequest()
+                .authenticated()
                 .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/perform_logout")).logoutSuccessUrl("/")
-                .invalidateHttpSession(true)
-                .deleteCookies("UISESSION");
+                .oauth2Login();
     }
-*/
+
+
 }
